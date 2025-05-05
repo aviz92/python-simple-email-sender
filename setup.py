11042cca7f25804b95598493617fb960f379d00b
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-05-05 21:02:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="python-simple-email-sender ",
+    name="python-simple-email-sender",
     version="0.1.0",
     author='Avi Zaguri',
     author_email="",
