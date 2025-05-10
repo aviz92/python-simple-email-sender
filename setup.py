434dafb5336c588e64e30bbaa20769fea3a185b1
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-05-10 12:09:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="python-simple-email-sender",
-    version="0.2.0",
+    version="0.2.1",
     author='Avi Zaguri',
     author_email="",
     description="Enhanced Gmail sender",
